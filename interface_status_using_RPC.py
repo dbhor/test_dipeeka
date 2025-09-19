3ed4a3f34633170db3145c9fd0ba3d703b9b767b
dbhor <148156365+dbhor@users.noreply.github.com>
2025-09-19 11:33:10 -0400

    Update interface_status_using_RPC.py

--- interface_status_using_RPC.py
+++ interface_status_using_RPC.py
@@ -1,7 +1,7 @@
 import jnpr.junos
 from jnpr.junos import Device
 from lxml import etree
-dev = Device(host="10.85.173.182", user="labroot", password="lab123")
+dev = Device(host="10.85.173.182", user="", password="")
 dev.open()
 rsp = dev.rpc.get_interface_information(terse=True,interface_name='ae201')
 print(etree.tostring(rsp))
