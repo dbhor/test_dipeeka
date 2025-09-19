f76c69e31983852412b077caa9e21999dc3de669
dbhor <148156365+dbhor@users.noreply.github.com>
2025-09-19 11:40:41 -0400

    Update xml_phy_port.py

--- xml_phy_port.py
+++ xml_phy_port.py
@@ -1,6 +1,6 @@
 from jnpr.junos import Device
 import xml.etree.ElementTree as ET
-dev= Device(host="10.85.173.182", user="labroot", password="lab123")
+dev= Device(host="10.5.3.2", user="", password="")
 dev.open()
 port_output= dev.rpc.get_interface_information()
 print(ET.tostring(port_output, encoding = "utf8", method= 'text'))
