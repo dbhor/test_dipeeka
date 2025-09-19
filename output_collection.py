b67477dfe8a8f3ee28c9dfa14be1e8012eca12fe
dbhor <148156365+dbhor@users.noreply.github.com>
2025-09-19 11:35:03 -0400

    Update output_collection.py

--- output_collection.py
+++ output_collection.py
@@ -3,7 +3,7 @@
 
 from jnpr.junos import Device
 from jnpr.junos.utils.start_shell import StartShell
-dev = Device(host="10.85.173.182", user="labroot", password="lab123")
+dev = Device(host="10.5.13.12", user="", password="")
 ss = StartShell(dev)
 ss.open()
 #ss.run('cli -c "request support information | save /var/tmp/information.txt"')
